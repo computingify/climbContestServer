ac329abacbf2fd11b6df4f9eb3324fc5f5d78543
adrienJouve <adrien.jouve@adn-dev.fr>
2025-03-12 15:11:48 +0100

    Add unit test on data base model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
 import pytest
+import sys
+import os
+
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from climb_contest import create_app, db
 from climb_contest.models import Climber, Bloc, Success
 
@@ -20,36 +26,37 @@ def app():
         db.session.remove()
         db.drop_all()
 
-
 @pytest.fixture()
 def client(app):
     return app.test_client()
-
 
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
 
 @pytest.fixture()
-def new_climber(db):
+def new_climber(app):
     """Creates a new climber in the database."""
-    climber = Climber(name="Test Climber", bib=123, club="Test Club", category="SNH")
-    db.session.add(climber)
-    db.session.commit()
-    return climber
+    with app.app_context():
+        climber = Climber(name="Test Climber", bib=123, club="Test Club", category="SNH")
+        db.session.add(climber)
+        db.session.commit()
+        return climber
 
 @pytest.fixture()
-def new_bloc(db):
+def new_bloc(app):
     """Creates a new bloc in the database."""
-    bloc = Bloc(tag="Test Bloc", number="456")
-    db.session.add(bloc)
-    db.session.commit()
-    return bloc
+    with app.app_context():
+        bloc = Bloc(tag="Test Bloc", number="456")
+        db.session.add(bloc)
+        db.session.commit()
+        return bloc
 
 @pytest.fixture()
-def new_success(db, new_climber, new_bloc):
+def new_success(app, new_climber, new_bloc):
     """Creates a new success in the database."""
-    success = Success(climber_id=new_climber.id, bloc_id=new_bloc.id)
-    db.session.add(success)
-    db.session.commit()
-    return success
+    with app.app_context():
+        success = Success(climber_id=new_climber.id, bloc_id=new_bloc.id)
+        db.session.add(success)
+        db.session.commit()
+        return success
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,119 +1,128 @@
-import pytest
 from datetime import datetime
 from climb_contest.models import db, Climber, Bloc, Success
-import os
 
-def test_create_climber(app, client, db):
+def test_create_climber(app):
     """Add climber in the database"""
     with app.app_context():
-        # Création d'un nouveau grimpeur
+        # Create a new climber
         climber = Climber(
             name="John Doe",
-            bib=1,
+            bib=2,
             club="Club A",
             category="SNH"
         )
 
-        # Ajout et validation dans la base de données
+        # Add and commit to the database
         db.session.add(climber)
         db.session.commit()
 
-        # Récupération du grimpeur depuis la base de données
+        # Retrieve the climber from the database
         saved_climber = Climber.query.filter_by(name="John Doe").first()
 
-        # Vérifications
+        # Verifications
         assert saved_climber is not None
         assert saved_climber.name == "John Doe"
-        assert saved_climber.bib == 1
+        assert saved_climber.bib == 2
         assert saved_climber.club == "Club A"
         assert saved_climber.category == "SNH"
 
-def test_create_bloc(app, client, db):
+def test_create_bloc(app):
     """Add bloc in the database"""
     with app.app_context():
-        # Création d'un nouveau bloc
+        # Create a new bloc
         bloc = Bloc(
             tag="Bloc A",
             number="001"
         )
 
-        # Ajout et validation dans la base de données
+        # Add and commit to the database
         db.session.add(bloc)
         db.session.commit()
 
-        # Récupération du bloc depuis la base de données
+        # Retrieve the bloc from the database
         saved_bloc = Bloc.query.filter_by(tag="Bloc A").first()
 
-        # Vérifications
+        # Verifications
         assert saved_bloc is not None
         assert saved_bloc.tag == "Bloc A"
         assert saved_bloc.number == "001"
 
-def test_create_success(app, client, db, new_climber, new_bloc):
+def test_create_success(app):
     """Add success in the database"""
     with app.app_context():
-        # Création d'un nouveau succès
+        # Create climber
+        climber = Climber(name="Alice Smith", bib=2, club="Club B", category="U18")
+
+        # Create blocs
+        bloc = Bloc(tag="A1", number="1")
+        db.session.add_all([bloc, climber])
+        db.session.commit()
+        # Create a new success
         success = Success(
-            climber_id=new_climber.id,
-            bloc_id=new_bloc.id,
-            timestamp=datetime.utcnow()  # Use datetime.utcnow() for timezone-naive timestamps
+            climber_id=climber.id,
+            bloc_id=bloc.id,
         )
 
-        # Ajout et validation dans la base de données
+        # Add and commit to the database
         db.session.add(success)
         db.session.commit()
 
-        # Récupération du succès depuis la base de données
-        saved_success = Success.query.filter_by(climber_id=new_climber.id).first()
+        # Retrieve the success from the database
+        saved_success = Success.query.filter_by(climber_id=climber.id).first()
 
-        # Vérifications
+        # Verifications
         assert saved_success is not None
-        assert saved_success.climber_id == new_climber.id
-        assert saved_success.bloc_id == new_bloc.id
+        assert saved_success.climber_id == climber.id
+        assert saved_success.bloc_id == bloc.id
         assert saved_success.timestamp is not None
 
-def test_2_success(app, client, db, new_climber):
+def test_2_success(app):
     """Add success in the database"""
     with app.app_context():
+        # Create climber
+        climber = Climber(name="Alice Smith", bib=2, club="Club B", category="U18")
+        db.session.add(climber)
+        db.session.commit()
+
         # Create blocs
         bloc1 = Bloc(tag="A1", number="1")
         bloc2 = Bloc(tag="B2", number="2")
         db.session.add_all([bloc1, bloc2])
         db.session.commit()
 
-        # Création de nouveaux succès
+        # Create new successes
         success1 = Success(
-            climber_id=new_climber.id,
+            climber_id=climber.id,
             bloc_id=bloc1.id,
             timestamp=datetime.utcnow()
         )
         success2 = Success(
-            climber_id=new_climber.id,
+            climber_id=climber.id,
             bloc_id=bloc2.id,
             timestamp=datetime.utcnow()
         )
 
-        # Ajout et validation dans la base de données
+        # Add and commit to the database
         db.session.add_all([success1, success2])
         db.session.commit()
 
-        # Récupération du succès depuis la base de données
-        saved_success = Success.query.filter_by(climber_id=new_climber.id).all()
+        # Retrieve the success from the database
+        saved_success = Success.query.filter_by(climber_id=climber.id).all()
 
-        # Vérifications
+        # Verifications
         assert len(saved_success) == 2
-        assert saved_success[0].climber_id == new_climber.id
+        assert saved_success[0].climber_id == climber.id
         assert saved_success[0].bloc_id == bloc1.id
         assert saved_success[0].timestamp is not None
-        assert saved_success[1].climber_id == new_climber.id
+        assert saved_success[1].climber_id == climber.id
         assert saved_success[1].bloc_id == bloc2.id
         assert saved_success[1].timestamp is not None
 
-def test_climber_successes(app, client, db):
+def test_climber_successes(app):
     """Test a climber succeeding on multiple blocs and retrieving all successes"""
     with app.app_context():
         # Create climber
-        climber = Climber(name="Alice Smith", bib=3, club="Club C", category="U18")
+        climber = Climber(name="Bob Johnson", bib=3, club="Club C", category="U20")
         db.session.add(climber)
         db.session.commit()
 
@@ -123,27 +132,25 @@ def test_climber_successes(app, client, db):
         db.session.add_all([bloc1, bloc2])
         db.session.commit()
 
-        # Création de nouveaux succès
+        # Create new successes
         success1 = Success(
             climber_id=climber.id,
             bloc_id=bloc1.id,
-            timestamp=datetime.utcnow()
         )
         success2 = Success(
             climber_id=climber.id,
             bloc_id=bloc2.id,
-            timestamp=datetime.utcnow()
         )
 
-        # Ajout et validation dans la base de données
+        # Add and commit to the database
         db.session.add_all([success1, success2])
         db.session.commit()
 
-        # Récupération des succès depuis la base de données
-        saved_climber = Climber.query.filter_by(name="Alice Smith").first()
+        # Retrieve the successes from the database
+        saved_climber = Climber.query.filter_by(name=climber.name).first()
         saved_successes = saved_climber.successes
 
-        # Vérifications
+        # Verifications
         assert len(saved_successes) == 2
         assert saved_successes[0].bloc_id == bloc1.id
         assert saved_successes[1].bloc_id == bloc2.id
