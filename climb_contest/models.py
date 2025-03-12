d50869bbd76fe6668848e1ea45af81a26b6be3ff
adrienJouve <adrien.jouve@adn-dev.fr>
2025-03-12 15:11:48 +0100

    Add unit test on data base model

--- climb_contest/models.py
+++ climb_contest/models.py
@@ -1,7 +1,6 @@
 from climb_contest import db
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
@@ -14,6 +13,12 @@ class Climber(db.Model, Base):
     club = Column(String(50))
     category = Column(String(5))
     successes = relationship('Success', backref='climber', lazy=True)
+    
+class Bloc(db.Model):
+    __tablename__ = 'bloc'
+    id = db.Column(Integer, primary_key=True)
+    tag = Column(String(50), unique=True, nullable=False)
+    number = Column(String(50), unique=True, nullable=False)
 
 class Success(db.Model, Base):
     __tablename__ = 'success'
@@ -21,9 +26,3 @@ class Success(db.Model, Base):
     climber_id = Column(Integer, ForeignKey('climber.id'), nullable=False)
     bloc_id = Column(Integer, ForeignKey('bloc.id'), nullable=False)
     timestamp = Column(DateTime, nullable=False, default=func.current_timestamp())
-    
-class Bloc(db.Model):
-    __tablename__ = 'bloc'
-    id = db.Column(Integer, primary_key=True)
-    tag = Column(String(50), unique=True, nullable=False)
-    number = Column(String(50), unique=True, nullable=False)
